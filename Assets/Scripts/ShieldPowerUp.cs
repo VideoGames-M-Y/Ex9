21a87a4a311512b0eadf326ab6edb19d24754ebd
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2025-01-14 22:05:37 +0200

    Update ShieldPowerUp.cs

--- Assets/Scripts/ShieldPowerUp.cs
+++ Assets/Scripts/ShieldPowerUp.cs
@@ -5,17 +5,17 @@ public class ShieldPowerUp : NetworkBehaviour
 {
     private Collider shieldCollider;
 
-    public override void Spawned() 
+    public override void Spawned()
     {
         // Ensure a collider is attached to the shield in the editor
         shieldCollider = GetComponent<Collider>();
     }
 
     // When the player collides with the shield, the shield will disappear
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter(Collider other)
     {
         // Check if the collider that collided with the shield is a player
-        if (other.CompareTag("Player")) 
+        if (other.CompareTag("Player"))
         {
             // Call the method to make the shield disappear and notify the player
             CollectShield(other.GetComponent<Player>());
