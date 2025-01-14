5158e5422f58cae1d5926e6563cac14aea4eff66
Yocheved Ofstein <157640249+YochevedOfstein@users.noreply.github.com>
2025-01-14 22:06:52 +0200

    Update SpawningLauncher.cs

--- Assets/Scripts/SpawningLauncher.cs
+++ Assets/Scripts/SpawningLauncher.cs
@@ -4,30 +4,39 @@ using Fusion;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
-// This class launches Fusion NetworkRunner, and also spanws a new avatar whenever a player joins.
+// This class launches Fusion NetworkRunner, and also spawns a new avatar whenever a player joins.
 public class SpawningLauncher : EmptyLauncher
 {
     [SerializeField] NetworkPrefabRef _playerPrefab;
     [SerializeField] Transform[] spawnPoints;
     private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
-    public override void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {
+
+    public override void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
         Debug.Log($"Player {player} joined");
-        bool isAllowedToSpawn = (runner.GameMode == GameMode.Shared)? 
-            (player == runner.LocalPlayer):   // in Shared mode, the local player is allowed to spawn.
-            runner.IsServer;                  // in Host or Server mode, only the server is allowed to spawn.
-        if (isAllowedToSpawn) {
+
+        bool isAllowedToSpawn = (runner.GameMode == GameMode.Shared)
+            ? (player == runner.LocalPlayer)  // In Shared mode, the local player is allowed to spawn.
+            : runner.IsServer;               // In Host or Server mode, only the server is allowed to spawn.
+
+        if (isAllowedToSpawn)
+        {
             // Create a unique position for the player
             Vector3 spawnPosition = spawnPoints[player.AsIndex % spawnPoints.Length].position;
-            //new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 0, 0);
+            // new Vector3((player.RawEncoded % runner.Config.Simulation.PlayerCount) * 3, 0, 0);
             NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, /*input authority:*/ player);
+
             // Keep track of the player avatars for easy access
             _spawnedCharacters.Add(player, networkPlayerObject);
         }
     }
-    
-    public override void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {
+
+    public override void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
         Debug.Log($"Player {player} left");
-        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject)) {
+
+        if (_spawnedCharacters.TryGetValue(player, out NetworkObject networkObject))
+        {
             runner.Despawn(networkObject);
             _spawnedCharacters.Remove(player);
         }
@@ -36,9 +45,23 @@ public class SpawningLauncher : EmptyLauncher
     [SerializeField] InputAction moveAction = new InputAction(type: InputActionType.Button);
     [SerializeField] InputAction shootAction = new InputAction(type: InputActionType.Button);
     [SerializeField] InputAction colorAction = new InputAction(type: InputActionType.Button);
-    private void OnEnable() { moveAction.Enable(); shootAction.Enable(); colorAction.Enable(); }
-    private void OnDisable() { moveAction.Disable(); shootAction.Disable(); colorAction.Disable();  }
-    void OnValidate() {
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        shootAction.Enable();
+        colorAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        shootAction.Disable();
+        colorAction.Disable();
+    }
+
+    void OnValidate()
+    {
         // Provide default bindings for the input actions. Based on answer by DMGregory: https://gamedev.stackexchange.com/a/205345/18261
         if (moveAction.bindings.Count == 0)
             moveAction.AddCompositeBinding("2DVector")
@@ -46,27 +69,34 @@ public class SpawningLauncher : EmptyLauncher
                 .With("Down", "<Keyboard>/downArrow")
                 .With("Left", "<Keyboard>/leftArrow")
                 .With("Right", "<Keyboard>/rightArrow");
+
         if (shootAction.bindings.Count == 0)
             shootAction.AddBinding("<Keyboard>/space");
+
         if (colorAction.bindings.Count == 0)
             colorAction.AddBinding("<Keyboard>/C");
     }
 
     NetworkInputData inputData = new NetworkInputData();
 
-    private void Update() {
-        if (shootAction.WasPressedThisFrame()) {
+    private void Update()
+    {
+        if (shootAction.WasPressedThisFrame())
+        {
             inputData.shootActionValue = true;
         }
-        if (colorAction.WasPressedThisFrame()) {
+
+        if (colorAction.WasPressedThisFrame())
+        {
             inputData.colorActionValue = true;
         }
     }
 
-    public override void OnInput(NetworkRunner runner, NetworkInput input) {
+    public override void OnInput(NetworkRunner runner, NetworkInput input)
+    {
         inputData.moveActionValue = moveAction.ReadValue<Vector2>();
-        input.Set(inputData);    // pass inputData by value 
-        inputData.shootActionValue = false; // clear shoot flag
-        inputData.colorActionValue = false; // clear shoot flag
+        input.Set(inputData);  // Pass inputData by value 
+        inputData.shootActionValue = false; // Clear shoot flag
+        inputData.colorActionValue = false; // Clear shoot flag
     }
 }
